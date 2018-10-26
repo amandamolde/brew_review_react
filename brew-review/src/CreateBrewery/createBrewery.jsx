f070fab19e6559bdcc181bf7e7545c3c10929326
amandamolde <amanda.molde@gmail.com>
2018-10-25 20:15:25 -0600

    Added select for state on create & edit brewery. On edit brewery, changed label of submit button to 'submit' and deleted the 'edit' in front of all the labels

--- brew-review/src/CreateBrewery/createBrewery.jsx
+++ brew-review/src/CreateBrewery/createBrewery.jsx
@@ -37,7 +37,59 @@ class CreateBrewery extends Component {
                     <Col md={4}>
                         <FormGroup>
                             <Label>State</Label>
-                            <Input type="text" name="state" onChange={this.updateBrewery}/>
+                            <Input type="select" name="state" onChange={this.updateBrewery}>
+                                <option selected disabled hidden>Select</option>
+                                <option>AL</option>
+                                <option>AK</option>
+                                <option>AZ</option>
+                                <option>AR</option>
+                                <option>CA</option>
+                                <option>CO</option>
+                                <option>CT</option>
+                                <option>DE</option>
+                                <option>FL</option>
+                                <option>GA</option>
+                                <option>HI</option>
+                                <option>ID</option>
+                                <option>IL</option>
+                                <option>IN</option>
+                                <option>IA</option>
+                                <option>KS</option>
+                                <option>KY</option>
+                                <option>LA</option>
+                                <option>ME</option>
+                                <option>MD</option>
+                                <option>MA</option>
+                                <option>MI</option>
+                                <option>MN</option>
+                                <option>MS</option>
+                                <option>MO</option>
+                                <option>MT</option>
+                                <option>NE</option>
+                                <option>NV</option>
+                                <option>NH</option>
+                                <option>NJ</option>
+                                <option>NM</option>
+                                <option>NY</option>
+                                <option>NC</option>
+                                <option>ND</option>
+                                <option>OH</option>
+                                <option>OK</option>
+                                <option>OR</option>
+                                <option>PA</option>
+                                <option>RI</option>
+                                <option>SC</option>
+                                <option>SD</option>
+                                <option>TN</option>
+                                <option>TX</option>
+                                <option>UT</option>
+                                <option>VT</option>
+                                <option>VA</option>
+                                <option>WA</option>
+                                <option>WV</option>
+                                <option>WI</option>
+                                <option>WY</option>
+                            </Input>
                             <FormText>Required</FormText>
                         </FormGroup>
                     </Col>
--- brew-review/src/EditBrewery/editBrewery.jsx
+++ brew-review/src/EditBrewery/editBrewery.jsx
@@ -7,36 +7,87 @@ const EditBrewery = (props) => {
         <div>
             <Form onSubmit={props.closeAndEdit}>
                 <FormGroup>
-                    <Label>Edit Name</Label>
+                    <Label>Name</Label>
                     <Input type="text" name="name" onChange={props.handleFormChange} value={props.breweryToEdit.name}/>
                     <FormText>Required</FormText>
                 </FormGroup>
                 <Row>
                     <Col md={8}>
                         <FormGroup>
-                            <Label>Edit City</Label>
+                            <Label>City</Label>
                             <Input type="text" name="city" onChange={props.handleFormChange} value={props.breweryToEdit.city}/>
                             <FormText>Required</FormText>
                         </FormGroup>
                     </Col>
                     <Col md={4}>
                         <FormGroup>
-                            <Label>Edit State</Label>
-                            <Input type="text" name="state" onChange={props.handleFormChange} value={props.breweryToEdit.state}/>
+                            <Label>State</Label>
+                            <Input type="select" name="state" onChange={props.handleFormChange} value={props.breweryToEdit.state}>
+                                <option>AL</option>
+                                <option>AK</option>
+                                <option>AZ</option>
+                                <option>AR</option>
+                                <option>CA</option>
+                                <option>CO</option>
+                                <option>CT</option>
+                                <option>DE</option>
+                                <option>FL</option>
+                                <option>GA</option>
+                                <option>HI</option>
+                                <option>ID</option>
+                                <option>IL</option>
+                                <option>IN</option>
+                                <option>IA</option>
+                                <option>KS</option>
+                                <option>KY</option>
+                                <option>LA</option>
+                                <option>ME</option>
+                                <option>MD</option>
+                                <option>MA</option>
+                                <option>MI</option>
+                                <option>MN</option>
+                                <option>MS</option>
+                                <option>MO</option>
+                                <option>MT</option>
+                                <option>NE</option>
+                                <option>NV</option>
+                                <option>NH</option>
+                                <option>NJ</option>
+                                <option>NM</option>
+                                <option>NY</option>
+                                <option>NC</option>
+                                <option>ND</option>
+                                <option>OH</option>
+                                <option>OK</option>
+                                <option>OR</option>
+                                <option>PA</option>
+                                <option>RI</option>
+                                <option>SC</option>
+                                <option>SD</option>
+                                <option>TN</option>
+                                <option>TX</option>
+                                <option>UT</option>
+                                <option>VT</option>
+                                <option>VA</option>
+                                <option>WA</option>
+                                <option>WV</option>
+                                <option>WI</option>
+                                <option>WY</option>
+                            </Input>
                             <FormText>Required</FormText>
                         </FormGroup>
                     </Col>
                 </Row>
                 <FormGroup>
-                    <Label>Edit Description</Label>
+                    <Label>Description</Label>
                     <Input type="textarea" name="description" onChange={props.handleFormChange} value={props.breweryToEdit.description}/>
                 </FormGroup>
                 <FormGroup>
-                    <Label>Edit Website</Label>
+                    <Label>Website</Label>
                     <Input type="text" name="website_url" onChange={props.handleFormChange} value={props.breweryToEdit.website_url}/>
                 </FormGroup>
                 <ModalFooter>
-                    <Button type="submit" onClick={props.editBreweryToggle}>Edit Brewery</Button>
+                    <Button type="submit" onClick={props.editBreweryToggle}>Submit</Button>
                     <Button type="submit" onClick={props.editBreweryToggle}>Cancel</Button>
                 </ModalFooter>
             </Form>
